1f93d52ca157dba35fc3cfb230ef2eb526c6d0fd
beatrizsantana-web <beatriz.sant.ana@escola.pr.gov.br>
2026-03-25 09:07:41 -0300

    FIZ TRABALHO PRIMEIRA PARTE

---
+++ index.js
@@ -0,0 +1,17 @@
+let num1 = 15;
+let num2 = 27;
+
+let soma = num1 + num2;
+console.log("soma:",soma);
+
+let subtracao = 50 - 25;
+console.log("subtração:", subtracao);
+
+let multiplicacao = 8 * 12;
+console.log("mutiplicação:", multiplicacao);
+
+let divisao = 100/4;
+console.log("divisão:", divisao);
+
+////////////////////////////////////////////////
+
